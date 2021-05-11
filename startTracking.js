877efbf4ee115a62d963aea39760818e58ef4ea3
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-05-11 12:22:25 -0700

    test

--- startTracking.js
+++ startTracking.js
@@ -2,30 +2,27 @@ AFRAME.registerComponent('start-tracking', {
     init: function() {
      // const ground = document.getElementById('ground')
      const text = document.getElementById('text')
-        // Create new entity for the new object
-        const CIV = document.createElement('a-entity')
+    const CIV = document.createElement('a-entity')
 
-        setTimeout(() => {
-            console.log('it worked');
-            text.setAttribute('value', 'New Text');
-          }, 5000)
+    setTimeout(() => {
+      console.log('it worked');
+      text.setAttribute('value', 'New Text');
+      CIV.setAttribute('position', '0 -1 0')
+      CIV.setAttribute('visible', 'false')
+      CIV.setAttribute('scale', '0.0001 0.0001 0.0001')
 
-        CIV.setAttribute('position', '0 -1 0')
-        CIV.setAttribute('visible', 'false')
-        CIV.setAttribute('scale', '0.0001 0.0001 0.0001')
+      CIV.setAttribute('gltf-model', '#3dmodel')
+      this.el.sceneEl.appendChild(CIV)
 
-        CIV.setAttribute('gltf-model', '#3dmodel')
-        this.el.sceneEl.appendChild(CIV)
-
-        CIV.addEventListener('model-loaded', () => {
-          // Once the model is loaded, we are ready to show it popping in using an animation
-          CIV.setAttribute('visible', 'true')
-          CIV.setAttribute('animation', {
-            property: 'scale',
-            to: '1 1 1',
-            easing: 'easeOutElastic',
-            dur: 800,
-          })
+      CIV.addEventListener('model-loaded', () => {
+        CIV.setAttribute('visible', 'true')
+        CIV.setAttribute('animation', {
+          property: 'scale',
+          to: '1 1 1',
+          easing: 'easeOutElastic',
+          dur: 800,
         })
-    }
-  })
+      })
+    }, 5000)
+  }
+})
