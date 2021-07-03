419603f408ab0368b02102eb1d71c52071f0161f
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-03 16:52:06 +0600

    test

--- startTracking.js
+++ startTracking.js
@@ -7,7 +7,7 @@ AFRAME.registerComponent('start-tracking', {
  
     setTimeout(() => {
 
-      CIV.setAttribute('position', '0 -1 0')
+      CIV.setAttribute('position', '5 -1 5')
       CIV.setAttribute('rotation', '0 180 0')
       CIV.setAttribute('visible', 'false')
       CIV.setAttribute('scale', '0.0001 0.0001 0.0001')
@@ -19,7 +19,7 @@ AFRAME.registerComponent('start-tracking', {
         CIV.setAttribute('visible', 'true')
         CIV.setAttribute('animation', {
           property: 'scale',
-          to: '1 1 1',
+          to: '50 50 50',
           easing: 'easeOutElastic',
           dur: 800,
         })
