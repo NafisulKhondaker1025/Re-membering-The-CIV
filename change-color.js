91a7e9445975c5c45ba4c7245f749cbd5d41fc5a
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-07 01:53:52 +0600

    test

--- change-color.js
+++ change-color.js
@@ -1,15 +1,22 @@
 AFRAME.registerComponent('change-color', {
     init: function () {
-      const slider = document.getElementById('slide')
-      slider.oninput = this.update.bind(this)
+        const container = document.getElementById('sidebar')
+        const slider = document.createElement('input')
+        slider.className = 'slider'
+        slider.setAttribute('type', 'range')
+        slider.setAttribute('min', '0')
+        slider.setAttribute('max', '255')
+        slider.setAttribute('value', '255')
+        container.appendChild(slider)
+        slider.oninput = this.update(slider.value).bind(this)
     },
-    update: function () {
-      var value = document.getElementById('slide').value
+    update: function (value) {
+      // var value = document.getElementById('slide').value
       const hexColor = "#" + value.toString(16) + value.toString(16) + value.toString(16)
       console.log(hexColor)
       var mesh = this.el.getObject3D('mesh');
       //if (!mesh) { return; }
-      //mesh.traverse(function (node) {
+      mesh.traverse(function (node) {
         if (node.isMesh) {
           node.material.color = new THREE.Color(hexColor);
           node.material.needsUpdate = true;
