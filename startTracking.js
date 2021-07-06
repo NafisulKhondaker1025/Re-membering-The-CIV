e8264b0d89d834bc1b664b09923cfa4499a42159
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-06 06:56:30 +0600

    test

--- startTracking.js
+++ startTracking.js
@@ -3,19 +3,17 @@ AFRAME.registerComponent('start-tracking', {
     const text = document.getElementById('loadingText')
     const CIV = document.createElement('a-entity')
     CIV.id = 'model' 
+    CIV.setAttribute('position', '0 0 0')
+    CIV.setAttribute('rotation', '0 180 0')
+    CIV.setAttribute('visible', 'false')
+    CIV.setAttribute('scale', '0.1 0.1 0.1')
+    CIV.setAttribute('gltf-model', '#3dmodel')
+    this.el.sceneEl.appendChild(CIV)
+    
     const container = document.getElementById('container')
     container.style.backgroundImage = "url('assets/stabilization.gif')"
- 
+
     setTimeout(() => {
-
-      CIV.setAttribute('position', '0 0 0')
-      CIV.setAttribute('rotation', '0 180 0')
-      CIV.setAttribute('visible', 'false')
-      CIV.setAttribute('scale', '0.1 0.1 0.1')
-
-      CIV.setAttribute('gltf-model', '#3dmodel')
-      this.el.sceneEl.appendChild(CIV)
-
       CIV.addEventListener('model-loaded', () => {
         CIV.setAttribute('visible', 'true')
         CIV.setAttribute('animation', {
