d094a9ac30d61d23a036832e7bf5e1e107551c99
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-07 01:44:57 +0600

    test

--- change-color.js
+++ change-color.js
@@ -6,13 +6,14 @@ AFRAME.registerComponent('change-color', {
     update: function () {
       var value = document.getElementById('slide').value
       const hexColor = "#" + value.toString(16) + value.toString(16) + value.toString(16)
+      console.log(hexColor)
       var mesh = this.el.getObject3D('mesh');
-      if (!mesh) { return; }
-      mesh.traverse(function (node) {
+      //if (!mesh) { return; }
+      //mesh.traverse(function (node) {
         if (node.isMesh) {
           node.material.color = new THREE.Color(hexColor);
           node.material.needsUpdate = true;
         }
-      });
+      //});
     }
   });
