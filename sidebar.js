7ab543cfc72c1c0a81af96255bf0a41f7e6ab3dd
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-06 07:00:32 +0600

    text

--- sidebar.js
+++ sidebar.js
@@ -35,8 +35,8 @@ AFRAME.registerComponent('populate-sidebar', {
     changeColor: function (value) {
         const hexColor = "#" + value.toString(16) + value.toString(16) + value.toString(16)
         const model = document.getElementById('model')
-        const mesh = model.getObject3D('mesh')
-        mesh.traverse(function (node) {
+        const modelMesh = model.getObject3D('mesh')
+        modelMesh.traverse((node) => {
             node.material.color = new THREE.Color(hexColor)
         })
     },
