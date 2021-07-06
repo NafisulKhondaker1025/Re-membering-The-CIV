3d6419a6d80eab06f53a8b9d3742e7e5bd6c0def
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-06 08:37:20 +0600

    test

--- startTracking.js
+++ startTracking.js
@@ -1,9 +1,11 @@
 AFRAME.registerComponent('start-tracking', {
     init: function() {
     const text = document.getElementById('loadingText')
-    const CIV = document.getElementById('model')
+    const CIV = document.createElement('a-entity')
+    CIV.id = 'model'
     CIV.setAttribute('gltf-model', '#3dmodel')
     CIV.setAttribute('visible', 'false')
+    this.el.sceneEl.appendChild(CIV) 
     const container = document.getElementById('container')
     container.style.backgroundImage = "url('assets/stabilization.gif')"
  
