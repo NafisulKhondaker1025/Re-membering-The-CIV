46477e5913a538cebd1d4eae1a47fc24d6758141
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-06 07:37:51 +0600

    test

--- startTracking.js
+++ startTracking.js
@@ -2,10 +2,7 @@ AFRAME.registerComponent('start-tracking', {
     init: function() {
     const text = document.getElementById('loadingText')
     const CIV = document.createElement('a-entity')
-    CIV.id = model 
-    CIV.setAttribute('gltf-model', '#3dmodel')
-    CIV.setAttribute('visible', 'false')
-    this.el.sceneEl.appendChild(CIV)
+    CIV.id = 'model' 
     const container = document.getElementById('container')
     container.style.backgroundImage = "url('assets/stabilization.gif')"
  
@@ -13,13 +10,20 @@ AFRAME.registerComponent('start-tracking', {
 
       CIV.setAttribute('position', '0 0 0')
       CIV.setAttribute('rotation', '0 180 0')
+      CIV.setAttribute('visible', 'false')
       CIV.setAttribute('scale', '0.1 0.1 0.1')
-      CIV.setAttribute('visible', 'true')
-      CIV.setAttribute('animation', {
-        property: 'scale',
-        to: '1 1 1',
-        easing: 'easeOutElastic',
-        dur: 800,
+
+      CIV.setAttribute('gltf-model', '#3dmodel')
+      this.el.sceneEl.appendChild(CIV)
+
+      CIV.addEventListener('model-loaded', () => {
+        CIV.setAttribute('visible', 'true')
+        CIV.setAttribute('animation', {
+          property: 'scale',
+          to: '1 1 1',
+          easing: 'easeOutElastic',
+          dur: 800,
+        })
       })
       text.setAttribute('visible', 'false')
       container.style.display = "none"
