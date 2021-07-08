6440bbbeb46f4a8d79ddbb8bc9b56b9f61606add
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-08 22:59:04 +0600

    test

--- change-color.js
+++ change-color.js
@@ -1,39 +1,39 @@
-AFRAME.registerComponent('change-color', {
-    init: function () {
-        const container = document.getElementById('sidebar')
-        const slider = document.createElement('input')
-        slider.className = 'slider'
-        slider.setAttribute('type', 'range')
-        slider.setAttribute('min', '0')
-        slider.setAttribute('max', '255')
-        slider.setAttribute('value', '255')
-        slider.setAttribute('step', '1')
-        container.appendChild(slider)
+// AFRAME.registerComponent('change-color', {
+//     init: function () {
+//         const container = document.getElementById('sidebar')
+//         const slider = document.createElement('input')
+//         slider.className = 'slider'
+//         slider.setAttribute('type', 'range')
+//         slider.setAttribute('min', '0')
+//         slider.setAttribute('max', '255')
+//         slider.setAttribute('value', '255')
+//         slider.setAttribute('step', '1')
+//         container.appendChild(slider)
 
-        const setColor = (value) => {
-            // var value = document.getElementById('slide').value
-            var hexColor = parseInt(value, 10).toString(16)
-            console.log(hexColor)
-            hexColor = "#" + hexColor + hexColor + hexColor
-            console.log(hexColor)
-            var mesh = this.el.getObject3D('mesh');
-            //if (!mesh) { return; }
-            mesh.traverse((node) => {
-                if (node.isMesh) {
-                node.material.color = new THREE.Color(hexColor);
-                node.material.needsUpdate = true;
-                }
-            });
-        }
+//         const setColor = (value) => {
+//             // var value = document.getElementById('slide').value
+//             var hexColor = parseInt(value, 10).toString(16)
+//             console.log(hexColor)
+//             hexColor = "#" + hexColor + hexColor + hexColor
+//             console.log(hexColor)
+//             var mesh = this.el.getObject3D('mesh');
+//             //if (!mesh) { return; }
+//             mesh.traverse((node) => {
+//                 if (node.isMesh) {
+//                 node.material.color = new THREE.Color(hexColor);
+//                 node.material.needsUpdate = true;
+//                 }
+//             });
+//         }
         
-        this.el.sceneEl.addEventListener('realityready', () => {
-            setColor("155")
-          })
+//         this.el.sceneEl.addEventListener('realityready', () => {
+//             setColor("155")
+//           })
         
-        slider.addEventListener('change', function() {setColor(slider.value)}, false)
-        slider.addEventListener('input', function() {setColor(slider.value)}, false)
-    }
-  });
+//         slider.addEventListener('change', function() {setColor(slider.value)}, false)
+//         slider.addEventListener('input', function() {setColor(slider.value)}, false)
+//     }
+//   });
 
 // AFRAME.registerComponent('change-color', {
 //     init: function () {
